077ef8575839865dc15d44d8d167eac6917db08b
mhaddaoui <mhaddaou@student.1337.ma>
2022-12-27 13:39:17 -0500

    mandatory part

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+service mysql start
+
+mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
+mysql -u root -e "USE '${MYSQL_DATABASE}'; GRANT ALL PRIVILEGES ON * TO '${MYSQL_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql -u root -e "alter user 'root'@'localhost' identified by '${WORDPRESS_ROOT_PASSWORD}'";
