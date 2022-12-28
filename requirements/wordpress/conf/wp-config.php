42cce83fb8e5965eded8ecbaebe4f314d6473979
mhaddaoui <mhaddaou@student.1337.ma>
2022-12-28 09:10:17 -0500

    mem

--- requirements/wordpress/conf/wp-config.php
+++
@@ -1,7 +0,0 @@
-define( 'DB_NAME', 'mhaddaou_db' );
-
-/** MySQL database username */
-define( 'DB_USER', 'mhaddaou' );
-
-/** MySQL database password */
-define( 'DB_PASSWORD','mhaddaou1337' );
--- requirements/wordpress/script.sh
+++
@@ -1,7 +0,0 @@
-echo "script is runing"
-wp core install --allow-root --url=${DOMAIN_NAME} --title=${WORDPRESS_NAME} --admin_user=${WORDPRESS_ROOT_LOGIN} --admin_password=${WORDPRESS_ROOT_PASSWORD} --admin_email=${WORDPRESS_ROOT_EMAIL}
-wp user create ${MYSQL_USER} ${WORDPRESS_USER_EMAIL} --user_pass=${MYSQL_PASSWORD} --role=author --allow-root
-service php7.3-fpm start
-service php7.3-fpm stop
-
-php-fpm7.3 -F
