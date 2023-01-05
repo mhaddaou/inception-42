7dcc6eca112d0b56df886b5978b0d1855ca39525
mhaddaoui <mhaddaou@student.1337.ma>
2023-01-05 10:14:23 -0500

    done

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,5 +4,7 @@ service mysql start
 
 mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
+# % To specify any valid host,
 mysql -u root -e "USE '${MYSQL_DATABASE}'; GRANT ALL PRIVILEGES ON * TO '${MYSQL_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+# ALTER USER statement changes the properties of an existing MySQL user.
 mysql -u root -e "alter user 'root'@'localhost' identified by '${WORDPRESS_ROOT_PASSWORD}';"
